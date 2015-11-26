a91b44e0648f87bf3bd472043b59c003d95d4427
Tokijin <toki@aaviah.com>
2015-11-25 22:58:57 -0800

    Stats changes
    The namespace should have been lowercase originally. Stats list has been
    added to the Games.

--- src/structures/Game.java
+++ src/structures/Game.java
@@ -1,5 +1,14 @@
 package structures;
 
+import java.util.ArrayList;
+import statistics.*;
+
 public class Game {
 
+	private ArrayList<AbstractGameStatistic> gameStats;
+	
+	public Game()
+	{
+		gameStats = new ArrayList<AbstractGameStatistic>();
+	}
 }

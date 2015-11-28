73d89b6dfb182f3e23528e2928fbb028a26b7543
Tokijin <toki@aaviah.com>
2015-11-27 22:20:10 -0800

    Added main and starting GUI
    Now there's something to run and the initial GUI layout is in place. The
    buttons and text field are where I was imagining they'd be, although
    it's not the most beautiful thing in the world.

---
+++ src/PenguinParserMain.java
@@ -0,0 +1,13 @@
+import visual.PenguinParser;
+
+public class PenguinParserMain {
+
+	private static final String DEFAULT_WINDOW_NAME = "Penguin Parser";
+	private static PenguinParser myParser;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		myParser = new PenguinParser( DEFAULT_WINDOW_NAME );
+	}
+
+}
